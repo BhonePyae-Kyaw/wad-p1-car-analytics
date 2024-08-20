74f60357092b60fdb0cbc5954db51b8f652e6d0b
Bhone Pyae Kyaw <137211983+BhonePyae-Kyaw@users.noreply.github.com>
2024-08-20 23:16:30 +0700

    Feat: React router set up with 2 pages
    Feat:
    - React router set up
    - React Dashboard component
    - React Highlight component
    
    Remove:
    Unnecessary components

--- car-analytics/src/main.jsx
+++ car-analytics/src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Dashboard from "./routes/dashboard";
+import Highlight from "./routes/highlight";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Dashboard />,
+  },
+  {
+    path: "/highlight",
+    element: <Highlight />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
---
+++ car-analytics/src/routes/highlight.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Highlight() {
+  return (
+    <div>
+      <p>Highlight</p>
+    </div>
+  );
+}
+
+export default Highlight;
